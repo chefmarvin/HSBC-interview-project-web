63895caf136e9c27bd0aa7c65552ade936a4c8a7
Marvin Lee <marvin@Marvins-MacBook-Pro.local>
2024-06-11 08:26:25 +0800

    feature: detail graph

--- src/components/SymbolDetail.tsx
+++ src/components/SymbolDetail.tsx
@@ -1,8 +1,92 @@
 import { useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import EChartsReactCore from 'echarts-for-react/lib/core'
+import * as echarts from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import { GridComponent, TooltipComponent, TitleComponent, ToolboxComponent, DataZoomComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
 import DataContext from '../DataContext.tsx'
 import { historyBySymbol, searchBySymbol } from "../api.tsx"
 import { transformYFinanceData } from "../utils.ts"
+import {format} from "date-fns";
+import styled from "styled-components"
+
+const DetailWrapper = styled.div`
+  min-width: 80vw;
+  .basic-info {
+    margin: 16px 0;
+    display: flex;
+    justify-content: space-between;
+    margin: 16px 0;
+    .basic-right {
+      text-align: right;
+      display: flex;
+      justify-content: flex-end;
+      .price {
+        font-size: 24px;
+        font-weight: 800;
+      }
+      .compare {
+        text-align: center;
+        font-size: 14px;
+        font-weight: 500;
+        color: #ffffff;
+        margin-right: 0px;
+        padding: 2px 8px;
+        border-radius: 8px;
+      }
+      p {
+        margin: 4px 0;
+      }
+    }
+    .basic-left {
+      text-align: left;
+      .symbol {
+        font-size: 28px;
+        font-weight: 1000;
+      }
+      .name {
+        font-size: 18px;
+        font-weight: 500;
+        color: #666;
+      }
+      .quote-type {
+        font-size: 14px;
+        font-weight: 500;
+        color: #ffffff;
+        margin-left: 0;
+        padding: 2px 8px;
+        border-radius: 8px;
+        background-color: #213547;
+      }
+      .currency {
+        font-size: 14px;
+        font-weight: 500;
+        color: #ffffff;
+        margin-left: 0;
+        padding: 2px 8px;
+        border-radius: 8px;
+        background-color: #888888;        
+      }
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      p {
+        margin: 4px 0;
+        span {
+          margin: 0 8px;
+        }
+      }
+    }
+  }
+`
+
+// Register the required components
+echarts.use(
+  [ LineChart, CanvasRenderer, GridComponent, TooltipComponent, TitleComponent, ToolboxComponent, DataZoomComponent ]
+);
 
 const SymbolDetail = () => {
   const { id } = useParams()
@@ -27,7 +111,7 @@ const SymbolDetail = () => {
 
       const history = await historyBySymbol({
         symbol: cursoredSymbol?.metadata?.symbol || id!,
-        period: (cursoredSymbol?.metadata?.validRanges && cursoredSymbol?.metadata?.validRanges[0]) || '1mo'
+        period: 'max'
       })
       history && setCursoredSymbolHistory && setCursoredSymbolHistory(history)
     };
@@ -36,21 +120,111 @@ const SymbolDetail = () => {
   }, [id])
 
   return (
-    <div>
-      <h1>Symbol Detail: {id}</h1>
+    <DetailWrapper>
       {cursoredSymbol && (
-        <div>
-          <h2>Cursored Symbol</h2>
-          <p>{JSON.stringify(cursoredSymbol)}</p>
+        <div className="basic-info">
+          <div className="basic-left">
+            <p className="symbol">{cursoredSymbol.info?.symbol}</p>
+            <p className="name">{cursoredSymbol.info?.longName}</p>
+            <p>
+              <span className="quote-type">{cursoredSymbol.info?.quoteType}</span>
+              <span className="currency">{cursoredSymbol.info?.currency}</span>
+            </p>
+          </div>
+          <div className="basic-right">
+            <p className="price">{cursoredSymbol.metadata?.regularMarketPrice}</p>
+            <p>
+              <span className="compare" style={{
+                backgroundColor: cursoredSymbol.info && cursoredSymbol.metadata && (cursoredSymbol.metadata.regularMarketPrice! - cursoredSymbol.info.previousClose!)! < 0 ? 'green' : 'red'
+              }}>
+                {cursoredSymbol.info && cursoredSymbol.metadata && (cursoredSymbol.metadata.regularMarketPrice! - cursoredSymbol.info.previousClose!).toFixed(2)}
+              </span>
+              <span className="compare" style={{
+                backgroundColor: cursoredSymbol.info && cursoredSymbol.metadata && (cursoredSymbol.metadata.regularMarketPrice! - cursoredSymbol.info.previousClose!)! < 0 ? 'green' : 'red'
+              }}>
+                {cursoredSymbol.info && cursoredSymbol.metadata && (((cursoredSymbol.metadata.regularMarketPrice! - cursoredSymbol.info.previousClose!) / cursoredSymbol.info.previousClose!) * 100).toFixed(2)}%
+              </span>
+            </p>
+          </div>
         </div>
       )}
       {cursoredSymbolHistory && (
         <div>
-          <h2>History Data</h2>
-          <p>{JSON.stringify(cursoredSymbolHistory)}</p>
+          <EChartsReactCore
+            echarts={echarts}
+            option={{
+              tooltip: {
+                trigger: 'axis',
+                formatter: function (params: any[]) {
+                  const data = params[0].data
+                  return `
+                    <div>
+                      Date: ${data[0]}<br/>
+                      Open: ${parseFloat(data[1]).toFixed(2)}<br/>
+                      Close: ${parseFloat(data[2]).toFixed(2)}<br/>
+                      Low: ${parseFloat(data[3]).toFixed(2)}<br/>
+                      High: ${parseFloat(data[4]).toFixed(2)}
+                    </div>
+                  `
+                },
+                position: (pt: number[]) => [pt[0], '10%']
+              },
+              xAxis: {
+                type: 'time',
+                boundaryGap: false
+              },
+              yAxis: {
+                type: 'value',
+                boundaryGap: [0, '100%']
+              },
+              dataZoom: [
+                {
+                  type: 'inside',
+                  start: 80,
+                  end: 100
+                },
+                {
+                  start: 80,
+                  end: 100
+                }
+              ],
+              grid: [{
+                show: false,
+                z: 0,
+                left: "60px",
+                top: "70px",
+                right: "0px",
+                bottom: "70px",
+                containLabel: false,
+                backgroundColor: "#000000",
+                borderWidth: 1,
+                borderColor: "#ccc",
+              }],
+              series: [
+                {
+                  name: 'Close Price Trend',
+                  type: 'line',
+                  smooth: true,
+                  symbol: 'none',
+                  areaStyle: {},
+                  data: cursoredSymbolHistory?.map(item => [
+                    format(new Date(item.Date!), 'yyyy-MM-dd'),
+                    item.Open,
+                    item.Close,
+                    item.Low,
+                    item.High
+                  ])
+                }
+              ]
+            }}
+            notMerge={true}
+            lazyUpdate={true}
+            onChartReady={() => console.log('ready')}
+          />
         </div>
       )}
-    </div>
+      <></>
+    </DetailWrapper>
   )
 }
 
